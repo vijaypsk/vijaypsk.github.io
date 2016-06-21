8d9a95c8157e3b59f0c6bdbbf1694047bb95bca0
Vijay Poluru <vijaypsk@gmail.com>
2016-06-21 04:34:41 +0000

    Moved files to a different directory

--- sampleapp1/app/indicatordef.component.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=../indicatordef.component.js.map
--- sampleapp1/app/indicatorlist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var indicatorlist_component_1 = require('./indicatorlist.component');
-exports.IndicatorlistComponent = indicatorlist_component_1.IndicatorlistComponent;
-//# sourceMappingURL=../../index.js.map
